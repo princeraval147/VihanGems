97dcfca1644467da1cfd7fb29754a26da6285dfc
princeraval147 <princeraval147@gmail.com>
2025-06-03 15:18:58 +0530

    Add whatsapp link

--- src/component/WhatsAppButton.jsx
+++ src/component/WhatsAppButton.jsx
@@ -4,9 +4,15 @@ export function WhatsAppButton() {
     return (
         <>
             <a
-                href="https://wa.me/+919879294083"
+                href="whatsapp://send?phone=+919879294083"
+                // onClick={(e) => {
+                //     // fallback for devices where whatsapp:// might not work
+                //     if (!navigator.userAgent.toLowerCase().includes('mobile')) {
+                //         window.open('https://wa.me/919879294083', '_blank');
+                //     }
+                // }}
                 className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
-                target="_blank"
+                // target="_blank"
                 rel="noopener noreferrer"
             >
                 <WhatsAppIcon />
