7809bd2537d5d972eceaed9993293451352c2e11
princeraval147 <princeraval147@gmail.com>
2025-06-03 15:33:43 +0530

    WhatsApp button for mobile and web

--- src/component/WhatsAppButton.jsx
+++ src/component/WhatsAppButton.jsx
@@ -1,18 +1,20 @@
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export function WhatsAppButton() {
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    const phone = '919879294083';
+    const message = 'Hello!';
+    const href = isMobile
+        ? `whatsapp://send?phone=${phone}&text=${encodeURIComponent(message)}`
+        : `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+
     return (
         <>
             <a
-                href="whatsapp://send?phone=+919879294083"
-                // onClick={(e) => {
-                //     // fallback for devices where whatsapp:// might not work
-                //     if (!navigator.userAgent.toLowerCase().includes('mobile')) {
-                //         window.open('https://wa.me/919879294083', '_blank');
-                //     }
-                // }}
+                href={href}
                 className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
-                // target="_blank"
                 rel="noopener noreferrer"
             >
                 <WhatsAppIcon />
