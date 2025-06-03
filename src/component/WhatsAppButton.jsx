34d65ebaad968ada66ffb37a8f444d264fbee466
princeraval147 <princeraval147@gmail.com>
2025-06-03 15:56:15 +0530

    Whatsapp Link

--- src/component/WhatsAppButton.jsx
+++ src/component/WhatsAppButton.jsx
@@ -1,20 +1,12 @@
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export function WhatsAppButton() {
-
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    const phone = '919879294083';
-    const message = 'Hello!';
-    const href = isMobile
-        ? `whatsapp://send?phone=${phone}&text=${encodeURIComponent(message)}`
-        : `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
-
-
     return (
         <>
             <a
-                href={href}
+                href="https://wa.me/+919879294083"
                 className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
+                target="_blank"
                 rel="noopener noreferrer"
             >
                 <WhatsAppIcon />
