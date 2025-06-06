5367cc84896bcb249afdd5b131c7920f565a5c31
princeraval147 <princeraval147@gmail.com>
2025-06-06 15:30:10 +0530

    FLoating Whatsapp Button

--- src/component/WhatsAppButton.jsx
+++ src/component/WhatsAppButton.jsx
@@ -1,17 +1,18 @@
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export function WhatsAppButton() {
+
     return (
-        <>
+        <div className="fixed bottom-6 right-6 z-50 flex flex-col items-center animate-float">
             <a
                 href="https://wa.me/+919879294083"
-                className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-lg flex items-center justify-center"
+                className="bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-2xl transition duration-300 ease-in-out"
                 target="_blank"
                 rel="noopener noreferrer"
+                aria-label="Chat with us on WhatsApp"
             >
                 <WhatsAppIcon />
             </a>
-        </>
-
+        </div>
     );
 }
