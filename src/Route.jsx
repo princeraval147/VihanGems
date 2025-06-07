c461a453da8dbe3bee47b0c37ee11fd4d4d5473f
princeraval147 <princeraval147@gmail.com>
2025-06-07 12:54:43 +0530

    Routes Manage

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,12 +4,14 @@ import AboutUsPage from "./component/AboutUs";
 import App from "./App";
 import HomePage from "./component/Home";
 import DiamondGallery from "./component/DiamondGallery";
+import EventDetail from "./component/EventDetail";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App />}>
             <Route index element={<HomePage />} />
             <Route path='about' element={<AboutUsPage />} />
+            <Route path='events' element={<EventDetail />} />
             <Route path='diamond-gallery' element={<DiamondGallery />} />
 
             <Route path='*' element={<NotFound />} />
