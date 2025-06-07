658604c871df8ea14a2ef093fa51a336808c1e76
princeraval147 <princeraval147@gmail.com>
2025-06-07 12:57:10 +0530

    Change Font Family

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -27,6 +27,7 @@ const Header = () => {
 
     return (
         <motion.header
+            style={{ fontFamily: "Playfair Display" }}
             initial={{ y: -100 }}
             animate={{ y: 0 }}
             transition={{ duration: 0.6, ease: "easeOut" }}
