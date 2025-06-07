b2d004b668063b3cc931c4ad7f58624c4ad51b7a
princeraval147 <princeraval147@gmail.com>
2025-06-07 09:07:55 +0530

    Add Scroll To Top

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './component/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './component/Footer'
+import ScrollToTop from './component/ScrollToTop'
 
 const App = () => {
 
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
