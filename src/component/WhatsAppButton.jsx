4edd31658ef9fa7c4918af7abf3836ae28d4af64
princeraval147 <princeraval147@gmail.com>
2025-06-08 12:09:05 +0530

    Whatsapp Button

--- src/component/WhatsAppButton.jsx
+++ src/component/WhatsAppButton.jsx
@@ -5,7 +5,7 @@ export function WhatsAppButton() {
     return (
         <div className="fixed bottom-6 right-6 z-50 flex flex-col items-center animate-float">
             <a
-                href="https://wa.me/+919879294083"
+                href="https://wa.me/+919999999999"
                 className="bg-green-500 hover:bg-green-600 text-white p-3 rounded-full shadow-2xl transition duration-300 ease-in-out"
                 target="_blank"
                 rel="noopener noreferrer"
