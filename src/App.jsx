579faea67fdbaa89464d936803f8912a66bfd221
princeraval147 <princeraval147@gmail.com>
2025-06-16 17:32:48 +0530

    Prevent Developer Tool

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './component/Header'
 import { Outlet } from 'react-router-dom'
 import Footer from './component/Footer'
 import ScrollToTop from './component/ScrollToTop'
 
 const App = () => {
+
+  useEffect(() => {
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+    document.addEventListener('contextmenu', handleContextMenu);
+    return () => {
+      document.removeEventListener('contextmenu', handleContextMenu);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (
+        e.key === 'F12' ||
+        (e.ctrlKey && e.shiftKey && e.key === 'I') ||
+        (e.ctrlKey && e.shiftKey && e.key === 'J') ||
+        (e.ctrlKey && e.key === 'U') ||
+        (e.ctrlKey && e.key === 'S') // disable Ctrl+S (optional)
+      ) {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+
 
   return (
     <>
