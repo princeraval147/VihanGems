1996a754b7913a05ee2577283a9917949308d90a
princeraval147 <princeraval147@gmail.com>
2025-06-21 15:05:00 +0530

    Add Icons

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,6 +5,10 @@ import XIcon from '@mui/icons-material/X';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { NavLink } from 'react-router-dom';
+import LocationPinIcon from '@mui/icons-material/LocationPin';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import EmailIcon from '@mui/icons-material/Email';
+import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 
 const Footer = () => {
     return (
@@ -14,24 +18,17 @@ const Footer = () => {
 
 
                 {/* Logo & Description */}
-                <div>
-                    <img src="/Images/CropLogo.png" alt="Vihan Gems" className="h-12 mb-4" />
+                <div className='flex flex-col items-center'>
+                    <NavLink to="/#home" className="flex items-center mb-2">
+                        <img src="/Images/VihanGems.png" alt="Vihan Gems" className="h-12 mb-2" />
+                    </NavLink>
                     <p className="text-sm leading-6">
-                        Vihan Gems is a premium diamond company specializing in precision, trust, and brilliance. Explore the legacy of sparkle.
+                        Vihan Gems is a premium diamond company specializing in precision, trust, and brilliance.
                     </p>
-                    <div className="flex gap-4 mt-4 flex-wrap">
-                        {/* <a href="https://facebook.com" target="_blank" rel="noreferrer" className="hover:text-blue-400">
-                            <FacebookIcon />
-                        </a> */}
-                        <a href="https://www.instagram.com/vihan_gems" target="_blank" rel="noreferrer" className="hover:text-pink-400">
+                    <div className="flex gap-4 mt-2 flex-wrap">
+                        <a href="https://www.instagram.com/vihan_gems" target="_blank" rel="noreferrer" className="hover:text-pink-400" aria-label='Instagram' title='Instagram'>
                             <InstagramIcon />
                         </a>
-                        {/* <a href="https://twitter.com" target="_blank" rel="noreferrer" className="hover:text-blue-300">
-                            <XIcon />
-                        </a>
-                        <a href="https://linkedin.com" target="_blank" rel="noreferrer" className="hover:text-blue-500">
-                            <LinkedInIcon />
-                        </a> */}
                         <a href="https://youtube.com/@bhavinshah6582?si=j_AET1ohA263z8vS" target="_blank" rel="noreferrer" className="hover:text-red-600">
                             <YouTubeIcon />
                         </a>
@@ -66,30 +63,47 @@ const Footer = () => {
                     <h4 className="font-semibold text-base md:text-lg mb-4">Contact</h4>
                     <ul className="space-y-3 text-sm">
                         <li>
-                            {/* Email: <a href="mailto:info@vihangems.com" target='_blank' className="hover:text-gray-300">info@vihangems.com</a> */}
-                            Email: <a href="mailto:Bhavinshah_4029@yahoo.in" target='_blank' className="hover:text-gray-300">
+                            <EmailIcon /> Email: {" "}
+                            <a href="mailto:Bhavinshah_4029@yahoo.in" target='_blank' className="hover:text-gray-300">
                                 Bhavinshah_4029@yahoo.in
                             </a>
                         </li>
                         <li>
-                            Phone: <a href="tel:+919879820993" className="hover:text-gray-300">+91 98798 20993</a>
+                            <LocalPhoneIcon /> Phone: <a href="tel:+919879820993" className="hover:text-gray-300">+91 98798 20993</a>
                         </li>
-                        <li>Customer Support: 10am - 7pm IST</li>
+
+
+                        <li><SupportAgentIcon /> Customer Support: 10am - 7pm IST</li>
                     </ul>
                 </div>
 
                 {/* Address */}
                 <div>
-                    <h4 className="font-semibold text-base md:text-lg mb-4">Address</h4>
+                    <h4 className="font-semibold text-base md:text-lg mb-4">Our Location</h4>
+                    <a
+                        href="https://maps.app.goo.gl/tScUet7GxLzWyo8B8?g_st=iw"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="flex items-start gap-2 text-sm hover:text-gray-300"
+                    >
+                        <LocationPinIcon className="mt-1" />
+                        <span>
+                            Vihan Gems Pvt. Ltd.<br />
+                            1st Floor Sankalp Building,<br />
+                            Beside Gopal Locho,<br />
+                            Mahidharpura, Surat, India - 395003
+                        </span>
+                    </a>
+                </div>
+
+                {/* <div>
                     <p className="text-sm leading-6">
                         Vihan Gems Pvt. Ltd.<br />
-                        {/*1st Floor, Solitaire Plaza,<br />
-                        BKC Road, Surat, India - 400051 */}
                         1st Floor Sankalp Building, <br />
                         Beside Gopal Locho, <br />
                         Mahidharpura, Surat, India - 395003
                     </p>
-                </div>
+                </div> */}
             </div>
 
             <div className="mt-10 border-t border-white/10 pt-6 text-center text-xs text-white/70 px-4">
